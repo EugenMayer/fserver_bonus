15233c8eb272c855ca5c5a55fe15e3da50e14fe7
Eugen Mayer <mayer@impressive-media.de>
2010-10-18 01:40:00 +0200

    6.1, working version

---
+++ fserver_bonus_project_short.tpl.php
@@ -0,0 +1,21 @@
+<project>
+  <title><?php echo $details['title'];?></title>
+  <short_name><?php echo $details['short_name'];?></short_name>
+  <link><?php echo $details['link'];?></link>
+  <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/"><?php echo $details['dw:creator']?></dc:creator>
+  <terms>
+    <?php
+    foreach($details['terms'] as $name => $value) {
+      echo "<term><name>$name</name><value>$value</value></term>";
+    }
+    ?>
+  </terms>
+  <project_status><?php echo $details['project_status']; ?></project_status>
+  <api_versions>
+    <?php
+    foreach($details['apis'] as $api) {
+      echo "<api_version>$api</api_version>";
+    }
+    ?>
+  </api_versions>
+ </project>
---
+++ views/fserver-bonus-project-list.tpl.php
@@ -0,0 +1,8 @@
+<projects>
+<?php 
+  foreach($projects as $project) {
+     echo theme('fserver_bonus_project_short', $project);
+  }
+?>
+</projects>
+
